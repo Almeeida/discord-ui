9158e090b5e985227382abf055f8756a58e9a186
Almeeida <rodrigofigueire19@gmail.com>
2020-06-18 10:00:55 +0100

    Updates

---
+++ precache-manifest.3b7d7980959cb3f1d78d644ad1d66b3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bbb3d7a819cd6ea3dd7b904ee9019e7",
+    "url": "/discord.almeidx.me/index.html"
+  },
+  {
+    "revision": "ca38823ab0f9b57a97cf",
+    "url": "/discord.almeidx.me/static/js/2.ac6acdf9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/discord.almeidx.me/static/js/2.ac6acdf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9078af3d1d4ee3def9f2",
+    "url": "/discord.almeidx.me/static/js/main.f92baa8f.chunk.js"
+  },
+  {
+    "revision": "1801e33b378ffe46e98d",
+    "url": "/discord.almeidx.me/static/js/runtime-main.6cfab551.js"
+  },
+  {
+    "revision": "f4990c85f2538b134c313763f3e1f19a",
+    "url": "/discord.almeidx.me/static/media/logo.f4990c85.png"
+  }
+]);
